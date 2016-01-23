8aa03f1982a9bd3f1f476f04829288740a9db21a
unknown <>
2016-01-23 16:02:11 +0800

    汉诺塔游戏 ’

---
+++ programing/Hanoi.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+def hanoi(a, b, c, n):
+    if n == 1:
+        print a, '->', c
+    else:
+        hanoi(a, c, b, n-1)
+        print a, '->', c
+        hanoi(b, a, c, n-1)
+n = int(raw_input(u'请输入汉诺塔的层数：'))
+print hanoi('a', 'b', 'c', n)
